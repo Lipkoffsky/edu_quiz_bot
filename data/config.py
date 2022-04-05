f8a23035328839d9db93769a9bbb235ca8fda88d
Lipkoffsky <63399954+Lipkoffsky@users.noreply.github.com>
2022-04-05 22:29:05 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -8,7 +8,3 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа s
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
-# ADMINS = [836221909]
-# BOT_TOKEN = "5046040497:AAEBQ39u5EtDnhSc7YOOHcIVHvDt5nMwXVc"
-# IP = "127.0.0.1"
-
